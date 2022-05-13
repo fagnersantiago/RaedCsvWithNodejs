54afb93c5738dc8f6f99d12b48c0c0489c595e2e
Fagner Santiago <fagner.santiago@hotmail.com>
2022-05-13 16:46:14 -0300

    add more fields

--- src/routes.ts
+++ src/routes.ts
@@ -2,20 +2,27 @@ import { Router, Request, Response } from "express";
 import multer from "multer";
 import { Readable } from "stream";
 import readline from "readline";
+
 const router = Router();
 
 const multerConfig = multer();
 
 interface Fields {
   contract_id: string;
-  plan_name: string;
+  plan: string;
+  city: string;
+  neighborhood: string;
+  address: string;
+  cellphone: string;
+  card: string;
 }
+
 router.post(
   "/contents",
   multerConfig.single("file"),
   async (request: Request, response: Response) => {
     const { file } = request;
-    const buffer = file?.buffer.toString("utf-8");
+    const buffer = file?.buffer.toString();
 
     const readalbeFile = new Readable();
 
@@ -30,14 +37,18 @@ router.post(
 
     for await (let line of fileLine) {
       const lineSplit = line.split(";");
-      const changePosition = (lineSplit[0] = lineSplit[1]);
+      const changePosition = lineSplit[1];
 
       fields.push({
         contract_id: changePosition,
-        plan_name: lineSplit[2],
+        plan: lineSplit[2],
+        city: lineSplit[4],
+        address: lineSplit[5],
+        neighborhood: lineSplit[6],
+        cellphone: lineSplit[7],
+        card: lineSplit[8],
       });
     }
-
     return response.json(fields);
   }
 );
